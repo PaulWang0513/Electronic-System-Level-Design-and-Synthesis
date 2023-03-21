1d3ea416abad9a4fcaf18cba9657373ef1842258
PaulWang0513 <paul05130125@gmail.com>
2023-03-22 00:23:59 +0800

    complete a good enough version of filter_with_buffer

---
+++ hw1/filter_with_buffer/MedianMeanFilter.h
@@ -0,0 +1,34 @@
+#ifndef MEDIANMEAN_FILTER_H_
+#define MEDIANMEAN_FILTER_H_
+#include <systemc>
+using namespace sc_core;
+
+#include "filter_def.h"
+
+class MedianMeanFilter : public sc_module {
+public:
+    sc_in_clk i_clk;
+    sc_in<bool> i_rst;
+    sc_in<bool> i_en_median;
+    sc_in<bool> i_en_mean;
+    sc_fifo_in<unsigned char> i_r;
+    sc_fifo_in<unsigned char> i_g;
+    sc_fifo_in<unsigned char> i_b;
+    sc_fifo_out<int> o_result;
+
+    SC_HAS_PROCESS(MedianMeanFilter);
+    MedianMeanFilter(sc_module_name n);
+    ~MedianMeanFilter() = default;
+
+private:
+    void do_median_filter();
+    void do_mean_filter();
+    int val[MASK_X * MASK_Y];   // make the size sufficient to store pixels for median filter
+    unsigned char buffer[2][MASK_Y][MASK_X];   // use the first 2D array to buffer the last 511 pixels of a row => xoo
+                                               //                                                                  xoo
+                                               //                                                                  xoo
+                                               // use the last 2D array to buffer the first pixel of the next row => xxx
+                                               //                                                                    ooo
+                                               //                                                                    ooo
+};
+#endif
---
+++ hw1/filter_with_buffer/build/CMakeFiles/median_mean.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/user/ee6470/Electronic-System-Level-Design-and-Synthesis/hw1/filter_with_buffer/MedianMeanFilter.cpp" "CMakeFiles/median_mean.dir/MedianMeanFilter.cpp.o" "gcc" "CMakeFiles/median_mean.dir/MedianMeanFilter.cpp.o.d"
+  "/home/user/ee6470/Electronic-System-Level-Design-and-Synthesis/hw1/filter_with_buffer/Testbench.cpp" "CMakeFiles/median_mean.dir/Testbench.cpp.o" "gcc" "CMakeFiles/median_mean.dir/Testbench.cpp.o.d"
+  "/home/user/ee6470/Electronic-System-Level-Design-and-Synthesis/hw1/filter_with_buffer/main.cpp" "CMakeFiles/median_mean.dir/main.cpp.o" "gcc" "CMakeFiles/median_mean.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
